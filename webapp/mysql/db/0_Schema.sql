3c089b9cd839107c506b3e47feadac9f362aa3ea
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-08-14 14:21:27 +0900

    Tweak schema

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -19,8 +19,7 @@ CREATE TABLE isuumo.estate
     features    VARCHAR(64)         NOT NULL,
     popularity  INTEGER             NOT NULL,
     desc_popularity INTEGER GENERATED ALWAYS AS (estate.popularity * (-1)) VIRTUAL,
-    INDEX idx_lat_lon(latitude, longitude),
-    -- `location` POINT AS (POINT(latitude, longitude))
+    INDEX idx_lat_lon(latitude, longitude)
 );
 
 CREATE TABLE isuumo.chair
