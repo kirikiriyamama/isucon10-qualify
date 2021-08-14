9ea201ec3b71a09eb740652d75e07a377b2789be
Yusuke Nakamura <yusuke1994525@gmail.com>
2021-08-14 12:46:54 +0900

    Use location column

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -450,7 +450,7 @@ class App < Sinatra::Base
 
     estates_in_polygon = []
     coordinates_to_text = "'POLYGON((%s))'" % coordinates.map { |c| '%f %f' % c.values_at(:latitude, :longitude) }.join(',')
-    sql2 = "SELECT * FROM estate WHERE id IN (?) AND ST_Contains(ST_PolygonFromText(#{coordinates_to_text}), POINT(latitude, longitude)) ORDER BY popularity DESC, id ASC"
+    sql2 = "SELECT * FROM estate WHERE id IN (?) AND ST_Contains(ST_PolygonFromText(#{coordinates_to_text}), `location`) ORDER BY popularity DESC, id ASC"
     area_estates = db.xquery(sql2, estates.map {|e| e[:id]}.join(','))
 
     nazotte_estates = area_estates.first(NAZOTTE_LIMIT)
